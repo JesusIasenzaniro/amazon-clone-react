9c5143017048ff3a1bd3b77580a2990a7eed52e9
JesusIasenzaniro <16jesusvicente@gmail.com>
2021-03-23 19:55:29 +0100

    Solving the render of products by departments

--- src/components/DepartmentProducts.js
+++ src/components/DepartmentProducts.js
@@ -3,80 +3,71 @@ import { Grid } from '@material-ui/core';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import { useProductsContext } from '../context/ProductsContext';
-import Loading from '../components/Loading';
-import Error from '../components/Error';
 import { Link } from 'react-router-dom';
 import '../components/CSS/DepartmentProducts.css';
 import Star from './GeneralStyles/StarStyled';
+
 export const DepartmentProducts = ({ productChange, productId, stars }) => {
-    const { products_loading: loading, products_error: error, products } = useProductsContext();
-
-    if (loading) {
-        return <Loading />;
-    }
-    if (error) {
-        return <Error />;
-    }
-
-    return (
-        <div>
-            <Carousel
-                className='container__carousel'
-                removeArrowOnDeviceType={['tablet', 'mobile']}
-                additionalTransfrom={0}
-                arrows
-                autoPlaySpeed={3000}
-                centerMode={false}
-                containerClass='container'
-                dotListClass=''
-                draggable
-                focusOnSelect={false}
-                infinite
-                itemClass=''
-                keyBoardControl
-                minimumTouchDrag={80}
-                renderButtonGroupOutside={false}
-                renderDotsOutside={false}
-                responsive={{
-                    desktop: {
-                        breakpoint: {
-                            max: 3000,
-                            min: 1024,
+    const { products } = useProductsContext();
+    const handleProducts = products.filter((product) => product.category === productChange);
+    if (handleProducts.length >= 3) {
+        return (
+            <div>
+                <Carousel
+                    className='container__carousel'
+                    removeArrowOnDeviceType={['tablet', 'mobile']}
+                    additionalTransfrom={0}
+                    arrows
+                    autoPlaySpeed={3000}
+                    centerMode={false}
+                    containerClass='container'
+                    dotListClass=''
+                    draggable
+                    focusOnSelect={false}
+                    infinite
+                    itemClass=''
+                    keyBoardControl
+                    minimumTouchDrag={80}
+                    renderButtonGroupOutside={false}
+                    renderDotsOutside={false}
+                    responsive={{
+                        desktop: {
+                            breakpoint: {
+                                max: 3000,
+                                min: 1024,
+                            },
+                            items: 3,
+                            partialVisibilityGutter: 40,
                         },
-                        items: 3,
-                        partialVisibilityGutter: 40,
-                    },
-                    mobile: {
-                        breakpoint: {
-                            max: 464,
-                            min: 0,
+                        mobile: {
+                            breakpoint: {
+                                max: 464,
+                                min: 0,
+                            },
+                            items: 1,
+                            partialVisibilityGutter: 30,
                         },
-                        items: 1,
-                        partialVisibilityGutter: 30,
-                    },
-                    tablet: {
-                        breakpoint: {
-                            max: 1024,
-                            min: 464,
+                        tablet: {
+                            breakpoint: {
+                                max: 1024,
+                                min: 464,
+                            },
+                            items: 2,
+                            partialVisibilityGutter: 30,
                         },
-                        items: 2,
-                        partialVisibilityGutter: 30,
-                    },
-                }}
-                // showDots={true}
-                sliderClass=''
-                slidesToSlide={1}
-                swipeable={true}
-            >
-                {products.map((product) => {
-                    const { id, image, category, name } = product;
-                    if (category === productChange && product.length >= 3) {
-                        if (id === productId) {
-                            return;
-                        }
-
-                        return (
-                            <div>
+                    }}
+                    // showDots={true}
+                    sliderClass=''
+                    slidesToSlide={1}
+                    swipeable={true}
+                >
+                    {products.map((product) => {
+                        const { id, image, category, name } = product;
+                        if (category === productChange) {
+                            if (id === productId) {
+                                return null;
+                            }
+                            return (
                                 <Grid key={id} className='container__carousel_products' container justify='center' alignItems='center'>
                                     <main>
                                         <div className='product__department__details'>
@@ -93,11 +84,13 @@ export const DepartmentProducts = ({ productChange, productId, stars }) => {
                                         </Link>
                                     </main>
                                 </Grid>
-                            </div>
-                        );
-                    }
-                })}
-            </Carousel>
-        </div>
-    );
+                            );
+                        }
+                    })}
+                </Carousel>
+            </div>
+        );
+    } else {
+        return null;
+    }
 };
